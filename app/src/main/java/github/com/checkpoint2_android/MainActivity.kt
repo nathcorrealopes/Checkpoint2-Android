97f84626483848b51a0e84493fcf1906279ae38b
Nathalia Lopes <correalopesnathali@gmail.com>
2024-10-07 22:25:25 -0300

    Prova checkpoint 2

---
+++ app/src/main/java/github/com/checkpoint2_android/MainActivity.kt
@@ -0,0 +1,32 @@
+package github.com.checkpoint2_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import github.com.checkpoint2_android.ui.theme.Checkpoint2AndroidTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+    }
+}
